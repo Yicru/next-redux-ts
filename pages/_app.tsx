00e7aefca3299e9cc71026260327520e889d9cbb
miyamae <miya.y.1201@gmail.com>
2019-08-13 11:16:54 +0900

    chore: headタグ追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Provider } from "react-redux";
 import withRedux from "next-redux-wrapper";
 import withReduxSaga from "next-redux-saga";
+import Head from "next/head";
 
 import createStore from "../store/store";
 import "styles/index.sass";
@@ -25,11 +26,21 @@ class MyApp extends App<MyAppProps> {
   render() {
     const { Component, pageProps, store } = this.props;
     return (
-      <Container>
-        <Provider store={store}>
-          <Component {...pageProps} />
-        </Provider>
-      </Container>
+      <>
+        <Head>
+          <title>Next Template</title>
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+            key="viewport"
+          />
+        </Head>
+        <Container>
+          <Provider store={store}>
+            <Component {...pageProps} />
+          </Provider>
+        </Container>
+      </>
     );
   }
 }
